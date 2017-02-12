33cdff13d7c32603cd26f8a65c0751573c8b9cda
ADITYA SINGH <singhaditya08aug@gmail.com>
2017-02-12 23:07:32 +0530

    Add files via upload

---
+++ packaging cupcakes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	for(int j=0;j<t;j++)
+	{
+		long long int n;
+		cin>>n;
+		long long int p=n/2+1;
+		cout<<p<<endl;
+	}
+	return 0;
+}
